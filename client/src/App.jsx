74a9c41ab528bedb4484337a39ebe496f2cf291f
lukas622 <bsds356@proton.me>
2025-06-26 13:58:04 +0600

    Backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-
-import Layout from './components/Layout'
-import Home from './components/Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomeHeader from './components/HomeHeader'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <HomeHeader/>
+      <main className='flex flex-col min-w-screen min-h-screen bg-cyan-50 text-center items-center'>
+        <div className='p-10 bg-amber-50 rounded-full  mt-30'>
+          <p className='font-bold text-black'>CarServiceRate is a platform which allows you to view detailed information about car service technicians.</p>
+          <br />
+          <p className='font-bold text-black'>Register to become a part of this community</p>
+        </div>
+      </main>
+    </>
   )
 }
 
