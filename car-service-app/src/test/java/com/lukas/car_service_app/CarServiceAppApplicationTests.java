15c209e3cf3455011719b8bed5f2243906b3ced0
D2JP <d2jp@TECHIN.INTERNAL>
2025-06-26 09:15:55 +0600

    Initial commit

---
+++ car-service-app/src/test/java/com/lukas/car_service_app/CarServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukas.car_service_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
